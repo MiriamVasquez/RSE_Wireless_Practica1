b03a7f72798c1cbe6b781be82a9b46098fbb8bce
unknown <ie728332@iteso.mx>
2026-04-12 22:54:46 -0600

    changer files names for better organization

--- frdmkw41z_wireless_examples_ieee_802_15_4_mwa_end_device_freertos/Debug/framework/ModuleInfo/ModuleInfo.d
+++
@@ -1,8 +0,0 @@
-framework/ModuleInfo/ModuleInfo.o framework/ModuleInfo/ModuleInfo.d: \
- ../framework/ModuleInfo/ModuleInfo.c \
- C:/Users/Miriam/Documents/MCUXpressoIDE_11.10.0_3148/REDES/frdmkw41z_wireless_examples_ieee_802_15_4_mwa_end_device_freertos/source/app_preinclude.h \
- ../framework/ModuleInfo/ModuleInfo.h \
- C:\Users\Miriam\Documents\MCUXpressoIDE_11.10.0_3148\REDES\frdmkw41z_wireless_examples_ieee_802_15_4_mwa_end_device_freertos\framework\common/EmbeddedTypes.h
-C:/Users/Miriam/Documents/MCUXpressoIDE_11.10.0_3148/REDES/frdmkw41z_wireless_examples_ieee_802_15_4_mwa_end_device_freertos/source/app_preinclude.h:
-../framework/ModuleInfo/ModuleInfo.h:
-C:\Users\Miriam\Documents\MCUXpressoIDE_11.10.0_3148\REDES\frdmkw41z_wireless_examples_ieee_802_15_4_mwa_end_device_freertos\framework\common/EmbeddedTypes.h:
--- frdmkw41z_wireless_examples_ieee_802_15_4_mwa_end_device_freertos/Debug/source/semihost_hardfault.d
+++
@@ -1,4 +0,0 @@
-source/semihost_hardfault.o source/semihost_hardfault.d: \
- ../source/semihost_hardfault.c \
- C:/Users/Miriam/Documents/MCUXpressoIDE_11.10.0_3148/REDES/frdmkw41z_wireless_examples_ieee_802_15_4_mwa_end_device_freertos/source/app_preinclude.h
-C:/Users/Miriam/Documents/MCUXpressoIDE_11.10.0_3148/REDES/frdmkw41z_wireless_examples_ieee_802_15_4_mwa_end_device_freertos/source/app_preinclude.h:
